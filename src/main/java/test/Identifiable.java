1d80641a1dbc5d5313b6b34546a4e7a3e0f927da
Thomas Iguchi <ti@nobu-games.com>
2017-07-28 13:15:36 -0700

    Documentation

--- src/main/java/test/Identifiable.java
+++ src/main/java/test/Identifiable.java
@@ -1,5 +1,12 @@
 package test;
 
+/**
+ * A resource type that can be identified by ID.
+ *
+ * @param <T>
+ *            Resource type.
+ * @see User
+ */
 public interface Identifiable<T> {
 	ID<T> getId();
 }
